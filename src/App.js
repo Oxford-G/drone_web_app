0ded92cd10a0c1e23f227a35f1be5f6a5328a397
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2022-06-18 18:30:03 +0100

    finish working on the tutorials page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Home from "./Home"
+import NavTop from './NavTop';
+import Tutorials from './Tutorials';
 
 function App() {
   return (
     <div className="App">
+      <NavTop />
       <Home />
+      <Tutorials />
+     
     </div>
   );
 }
