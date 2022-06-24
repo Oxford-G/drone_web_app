be58345a2f4054301825970abb0fdbecafe4e180
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2022-06-24 10:43:46 +0100

    lift states to app comp for general use

--- src/App.js
+++ src/App.js
@@ -1,20 +1,81 @@
+import { useState } from 'react';
 import './App.css';
-import Home from "./Home"
-import NavTop from './NavTop';
-import Tutorials from './Tutorials';
+import Home from "./Home";
 import Blogs from './Blogs';
-import Contact from './Contact'
-import MobileNav from './Mobile_Nav'
+import NavTop from './NavTop';
+import Contact from './Contact';
+import MobileNav from './Mobile_Nav';
+import TabletNav from './TabletNav';
+import Tutorials from './Tutorials';
 
-function App() {
+const App = () => {
+
+  const [navBar, setNavBar] = useState(false)
+  const [navBarT, setNavBarT] = useState(false)
+  const [navBarM, setNavBarM] = useState(false)
+  const [Toggle, setToggle] = useState(false)
+
+  const changeBackground = () => {
+    if ((window.scrollY >= 830) && (window.scrollY <= 1470)) {
+      setNavBar(true)
+    } else if ((window.scrollY >= 1471) && (window.scrollY <= 2200)) {
+      setNavBar('navImgBlack')
+    } else if ((window.scrollY >= 2201) && (window.scrollY <= 2980)) {
+      setNavBar(true)
+    } else if (window.scrollY >= 2981) {
+      setNavBar('navImgBrown')
+    }else {
+      setNavBar(false)
+    } 
+  }
+
+  const changeBackgroundTablet = () => {
+    if ((window.scrollY >= 510) && (window.scrollY <= 1091)) {
+      setNavBarT(true)
+    } else if ((window.scrollY >= 1092) && (window.scrollY <= 1850)) {
+      setNavBarT('navImgBlack')
+    } else if ((window.scrollY >= 1851) && (window.scrollY <= 2200)) {
+      setNavBarT(true)
+    } else if (window.scrollY >= 2201) {
+      setNavBarT('navImgBrown')
+    }else {
+      setNavBarT(false)
+    } 
+  }
+
+  const changeBackgroundMobile = () => {
+    if ((window.scrollY >= 510) && (window.scrollY <= 1091)) {
+      setNavBarM(true)
+    } else if ((window.scrollY >= 1092) && (window.scrollY <= 1850)) {
+      setNavBarM('navImgBlack')
+    } else if ((window.scrollY >= 1851) && (window.scrollY <= 2200)) {
+      setNavBarM(true)
+    } else if (window.scrollY >= 2201) {
+      setNavBarM('navImgBrown')
+    } else {
+      setNavBarM(false)
+    }
+  }
+
+  const toggleNav = () => {
+    setToggle(!Toggle)
+  }
+
+  const closeMenu = () => setToggle(false)
+
+  window.addEventListener('scroll', changeBackground)
+  window.addEventListener('scroll', changeBackgroundMobile)
+  window.addEventListener('scroll', changeBackgroundTablet)
+
   return (
     <div className="App">
-      <NavTop />
-      <MobileNav />
-      <Home />
-      <Tutorials />
-      <Blogs />
-      <Contact />
+      <NavTop navBar={navBar} Toggle={Toggle} toggleNav={toggleNav} closeMenu={closeMenu}/>
+      <TabletNav navBarT={navBarT} Toggle={Toggle} toggleNav={toggleNav} closeMenu={closeMenu}/>
+      <MobileNav navBarM={navBarM} Toggle={Toggle} toggleNav={toggleNav} closeMenu={closeMenu}/>
+      <Home closeMenu={closeMenu}/>
+      <Tutorials closeMenu={closeMenu}/>
+      <Blogs closeMenu={closeMenu}/>
+      <Contact closeMenu={closeMenu}/>
     </div>
   );
 }
